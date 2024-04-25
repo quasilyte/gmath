639671a2b8dcdf73595d005091c819e65a128b9c
Iskander Sharipov <quasilyte@gmail.com>
2024-04-25 15:46:38 +0400

    make Rect.Center return the correct values when Min is not at (0,0)
    This is a breaking change, but the original behavior
    was simply incorrect.

--- rect.go
+++ rect.go
@@ -5,17 +5,27 @@ type Rect struct {
 	Max Vec
 }
 
-func (r Rect) Width() float64  { return r.Max.X - r.Min.X }
+func (r Rect) Width() float64 { return r.Max.X - r.Min.X }
+
 func (r Rect) Height() float64 { return r.Max.Y - r.Min.Y }
 
+// Center returns the center point of this rectangle.
+//
+// This center point may need some rounding,
+// since a rect of a 3x3 size would return {1.5, 1.5}.
 func (r Rect) Center() Vec {
-	return Vec{X: r.Max.X / 2, Y: r.Max.Y / 2}
+	return Vec{
+		X: r.Max.X - r.Width()*0.5,
+		Y: r.Max.Y - r.Height()*0.5,
+	}
 }
 
 func (r Rect) X1() float64 { return r.Min.X }
+
 func (r Rect) Y1() float64 { return r.Min.Y }
 
 func (r Rect) X2() float64 { return r.Max.X }
+
 func (r Rect) Y2() float64 { return r.Max.Y }
 
 func (r Rect) IsEmpty() bool {
