2271c12cc68ddf8461a54862e0c0a1ac87de3183
Iskander Sharipov <quasilyte@gmail.com>
2024-05-19 11:35:39 +0400

    add some useful method to the Range type

--- range.go
+++ range.go
@@ -8,3 +8,15 @@ type Range[T numeric] struct {
 func MakeRange[T numeric](min, max T) Range[T] {
 	return Range[T]{Min: min, Max: max}
 }
+
+func (rng Range[T]) IsZero() bool {
+	return rng == Range[T]{}
+}
+
+func (rng Range[T]) IsValid() bool {
+	return rng.Max >= rng.Min
+}
+
+func (rng Range[T]) Len() int {
+	return int(rng.Max - rng.Min)
+}
