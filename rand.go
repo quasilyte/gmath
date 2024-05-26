d84241cee2c2746c6dca56acd1f8419b4161dbc9
Iskander Sharipov <quasilyte@gmail.com>
2024-05-26 17:19:12 +0400

    add RandIterate function

--- rand.go
+++ rand.go
@@ -71,6 +71,40 @@ func RandElem[T any](r *Rand, slice []T) (elem T) {
 	return slice[r.rng.Intn(len(slice))]
 }
 
+func RandIterate[T any](r *Rand, slice []T, f func(x T) bool) T {
+	var result T
+
+	if len(slice) == 0 {
+		return result
+	}
+	if len(slice) == 1 {
+		// Don't use rand() if there is only 1 element.
+		x := slice[0]
+		if f(x) {
+			result = x
+		}
+		return result
+	}
+
+	var slider Slider
+	slider.SetBounds(0, len(slice)-1)
+	slider.TrySetValue(r.IntRange(0, len(slice)-1))
+	inc := r.Bool()
+	for i := 0; i < len(slice); i++ {
+		x := slice[slider.Value()]
+		if inc {
+			slider.Inc()
+		} else {
+			slider.Dec()
+		}
+		if f(x) {
+			result = x
+			break
+		}
+	}
+	return result
+}
+
 func Shuffle[T any](r *Rand, slice []T) {
 	r.rng.Shuffle(len(slice), func(i, j int) {
 		slice[i], slice[j] = slice[j], slice[i]
