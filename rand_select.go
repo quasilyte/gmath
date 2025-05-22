15fde51d84c60802b10acc809c1b736afa00f4b7
Iskander Sharipov <quasilyte@gmail.com>
2025-05-23 01:45:52 +0400

    add alternative rand select sort order

--- rand_select.go
+++ rand_select.go
@@ -3,6 +3,10 @@ package gmath
 type RandSelectConfig[T any] struct {
 	Rand *Rand
 
+	// Whether higher weights should end up in
+	// the beginning of the slice.
+	HigherFirst bool
+
 	Options []RandSelectOption[T]
 }
 
@@ -25,22 +29,49 @@ func RandSelect[T any](config RandSelectConfig[T]) {
 	options := config.Options
 
 	totalWeight := 0.0
-	for _, o := range config.Options {
+	for _, o := range options {
 		totalWeight += o.Weight
 	}
 
-	for i := len(options) - 1; i > 0; i-- {
-		r := config.Rand.Float() * totalWeight
+	if config.HigherFirst {
+		randSelectHigherFirst(config.Rand, totalWeight, options)
+	} else {
+		randSelectLowerFirst(config.Rand, totalWeight, options)
+	}
+}
+
+func randSelectHigherFirst[T any](rand *Rand, totalWeight float64, options []RandSelectOption[T]) {
+	for i := 0; i < len(options)-1; i++ {
+		r := rand.Float() * totalWeight
 		j := 0
 
-		// Options are not sorted. Have to use a linear search.
+		sum := 0.0
+		for j = i; j < len(options); j++ {
+			sum += options[j].Weight
+			if sum >= r {
+				break
+			}
+		}
+
+		w := options[j].Weight
+		options[i], options[j] = options[j], options[i]
+		totalWeight -= w
+	}
+}
+
+func randSelectLowerFirst[T any](rand *Rand, totalWeight float64, options []RandSelectOption[T]) {
+	for i := len(options) - 1; i > 0; i-- {
+		r := rand.Float() * totalWeight
+		j := 0
+
 		sum := options[0].Weight
 		for sum < r {
 			j++
 			sum += options[j].Weight
 		}
 
+		w := options[j].Weight
 		options[i], options[j] = options[j], options[i]
-		totalWeight -= options[i].Weight
+		totalWeight -= w
 	}
 }
